edb7997825959a963e0d9c18270c5af3feaf190e
Souleymane Diarra <souleymane73495256@gmail.com>
2025-01-12 20:11:14 +0000

    Premier commit

--- array.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo strcmp("apple", "banana"); // Résultat : -1 (apple < banana)
-echo '<br>';
-echo strcmp("apple", "applb");  // Résultat : 0 (apple == apple)
-echo '<br>';
-echo strcmp("banana", "apple"); // Résultat : 1 (banana > apple)
-echo '<br>';
-?>
